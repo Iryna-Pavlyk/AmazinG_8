8981578b477aeaede9bf523a24c0f48182825e5d
DeLaVoroDN <igrovoy88888@gmail.com>
2024-03-15 21:05:55 +0200

    Up

--- src/js/accordeon.js
+++ src/js/accordeon.js
@@ -2,20 +2,32 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const accordionItems = document.querySelectorAll('.accordion-container .ac');
-  // Устанавливаем начальное состояние стрелок для каждого элемента аккордеона
-  accordionItems.forEach(item => setInitialArrowIcons(item));
+  // Инициализация аккордеона для секции FAQ
+  initializeAccordion('.accordion-container');
 
-  // Инициализация аккордеона с переопределением поведения стрелок при открытии и закрытии
-  new Accordion('.accordion-container', {
-    duration: 400,
-    showMultiple: true,
-    onOpen: element => toggleArrowIcons(element),
-    onClose: element => toggleArrowIcons(element),
-  });
+  // Инициализация аккордеона для секции About
+  initializeAccordion('.accordion-container-about');
 });
 
-// Устанавливаем начальное состояние стрелок
+function initializeAccordion(selector) {
+  const accordionItems = document.querySelectorAll(`${selector} .ac`);
+  accordionItems.forEach(item => setInitialArrowIcons(item));
+
+  new Accordion(selector, {
+    duration: 400,
+    showMultiple: true,
+    onOpen: element => {
+      toggleArrowIcons(element);
+      setAriaExpanded(element, true); // Устанавливаем aria-expanded в true
+    },
+    onClose: element => {
+      toggleArrowIcons(element);
+      setAriaExpanded(element, false); // Устанавливаем aria-expanded в false
+    },
+  });
+}
+
+// Устанавливаем начальное состояние стрелок для каждого элемента аккордеона
 function setInitialArrowIcons(item) {
   const arrowDown = item.querySelector('.arrow-down');
   const arrowUp = item.querySelector('.arrow-up');
@@ -23,11 +35,17 @@ function setInitialArrowIcons(item) {
   arrowUp.style.display = 'none'; // Скрыть стрелку вверх
 }
 
-// Переключаем состояние стрелок
+// Переключаем состояние стрелок при открытии/закрытии аккордеона
 function toggleArrowIcons(currentElement) {
   const arrowDown = currentElement.querySelector('.arrow-down');
   const arrowUp = currentElement.querySelector('.arrow-up');
   arrowDown.style.display =
     arrowDown.style.display === 'none' ? 'block' : 'none';
   arrowUp.style.display = arrowUp.style.display === 'none' ? 'block' : 'none';
+}
+
+// Функция для установки значения aria-expanded
+function setAriaExpanded(element, expanded) {
+  const triggerButton = element.querySelector('.ac-trigger');
+  triggerButton.setAttribute('aria-expanded', expanded);
 }
