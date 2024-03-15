dd4f41bc34be801c8a5cd41b97571ad9cfdd1f94
mooneryy <yelisyeyevaa@gmail.com>
2024-03-15 13:25:47 +0200

    +

---
+++ src/js/reviews.js
@@ -0,0 +1,55 @@
+// Функція для отримання відгуків з сервера
+async function fetchReviews() {
+    try {
+        const response = await fetch('https://portfolio-js.b.goit.study/api/reviews');
+        const data = await response.json();
+
+        if (data.error) {
+            showErrorMessage(data.error);
+        } else {
+            showReviews(data);
+        }
+    } catch (error) {
+        showErrorMessage('Failed to fetch reviews. Please try again later.');
+    }
+}
+
+// Функція для відображення відгуків
+function showReviews(reviews) {
+    const reviewsList = document.getElementById('reviewsList');
+
+    // Очищення вмісту списку перед додаванням нових відгуків
+    reviewsList.innerHTML = '';
+
+    if (reviews.length === 0) {
+        reviewsList.innerHTML = '<div class="swiper-slide">Not found</div>';
+    } else {
+        // Додавання відгуків до списку
+        reviews.forEach(review => {
+            const slide = document.createElement('li');
+            slide.classList.add('swiper-slide');
+            slide.innerHTML = `
+                <div class="review-card" id="review_${review._id}"> 
+                    <div class="avatar">
+                        <img class="review-img" src="${review.avatar_url}" alt="Avatar of ${review.author}">
+                    </div>
+                    <div class="review-content">
+                        <p class="author">${review.author}</p>
+                        <p class="text">${review.review}</p>
+                    </div>
+                </div>`;
+            reviewsList.appendChild(slide);
+        });
+    }
+}
+
+// Функція для відображення повідомлення про помилку
+function showErrorMessage(message) {
+    alert(message);
+}
+
+
+
+// Виклик функції отримання відгуків при завантаженні сторінки
+document.addEventListener('DOMContentLoaded', fetchReviews);
+
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,1 +1,29 @@
-// import Swiper from 'swiper';
+// Ініціалізація Swiper
+import Swiper from 'swiper';
+import 'swiper/css';
+
+
+export function initializeSwiper() {
+    const swiper = new Swiper('.swiper-container', {
+        spaceBetween: 35,
+        grabCursor: true,
+        keyboard: {
+            enabled: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            375: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1280: {
+                slidesPerView: 4,
+            },
+        },
+    });
+}
